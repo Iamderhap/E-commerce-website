6594583f84547fdf429c7ee756f7a258092c9499
unknown <happiness9979@gmail.com>
2024-08-24 06:40:38 +0200

    Hero updated

--- src/app/Components/Nav.jsx
+++ src/app/Components/Nav.jsx
@@ -5,6 +5,7 @@ import { FaRegHeart } from "react-icons/fa";
 import { LuShoppingCart } from "react-icons/lu";
 import { HiMenuAlt1 } from "react-icons/hi";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdOutlineMail } from "react-icons/md";
 import Image from 'next/image'
 
 const Nav = () => {
@@ -12,13 +13,12 @@ const Nav = () => {
         <div className='px-[70px] bg-black py-[10px] gap-[10px]'>
           <nav className=' flex justify-around items-center py-[10px] border-b-2 border-b-[grey]'>
                 <div className=''>
-                    <Image className='h-[7vh]' src='/images/krisppy-logo.jpg' width={100} height={5}/>
+                    <Image className='h-[7vh]' src='/images/krisppy-logo.jpg' width={200} height={5}/>
                 </div>
                 <div>
-                    <form className='flex items-center '>
-                        <input className='border-[#FFD700] border-2 py-[10px] px-[7px] focus:outline-none' type="text" name="items" id="" placeholder='Search For items...'/>
-                        {/* <input className='border-[yellow] border-2 py-[10px] px-[7px] focus:outline-none' type="text" name="categories" id="" placeholder='All Categories'/> */}
-                        <select className='cursor-pointer'  name="" id="">
+                    <form className='flex items-center'>
+                        <input className='border-[#FFD700] cursor-pointer border-[1px] py-[10px] w-[30vw] px-[10px] rounded-tl-lg rounded-bl-lg focus:outline-none' type="text" name="items" id="" placeholder='Search For items...'/>
+                        <select className='cursor-pointer border-[#FFD700] border-[1px] py-[11px] px-[7px] focus:outline-none'  name="" id="">
                           <option value="">jewelries</option>
                           <option value="">jewelries</option>
                           <option value="">jewelries</option>
@@ -26,27 +26,28 @@ const Nav = () => {
                           <option value="">jewelries</option>
 
                         </select>
-                        <button className='bg-[#FFD700] px-[12px] py-[15px]'><FiSearch /></button>
+                        <button className='bg-[#FFD700] px-[12px] py-[15px] rounded-br-lg rounded-tr-lg '><FiSearch /></button>
                     </form>
                 </div>
-                <ul className='flex gap-[15px] text-white'>
-                    <li className='flex items-center gap-[7px]'><VscAccount />Account</li>
-                    <li className='flex items-center gap-[7px]'><FaRegHeart />Wishlist</li>
-                    <li className='flex items-center gap-[7px]'><LuShoppingCart />Cart</li>
+                <ul className='flex gap-[15px] text-white items-center'>
+                    <li className='flex items-center gap-[7px] hover:text-[#FFD700] hover:cursor-pointer'><VscAccount />Account</li>
+                    <li className='flex items-center gap-[7px] hover:text-[#FFD700] hover:cursor-pointer'><FaRegHeart />Wishlist</li>
+                    <li className='flex items-center gap-[7px] hover:text-[#FFD700] hover:cursor-pointer'><LuShoppingCart />Cart</li>
                 </ul>
           </nav>
-          <div className='flex justify-around py-[20px] gap-[20%]'>
+          <div className='flex justify-around py-[20px]'>
             <div className='text-white border-white border-2 px-[5px] py-[3px]'>
                 <HiMenuAlt1 />
             </div>
             <ul className='flex gap-[20px] text-white'>
-                <li className='flex items-center gap-[5px] font-semibold'>Home<MdOutlineKeyboardArrowDown /></li>
-                <li className='flex items-center gap-[5px] font-semibold'>Category<MdOutlineKeyboardArrowDown /></li>
-                <li className='flex items-center gap-[5px] font-semibold'>Products<MdOutlineKeyboardArrowDown /></li>
-                <li className='flex items-center gap-[5px] font-semibold'>Pages<MdOutlineKeyboardArrowDown /></li>
-                <li className='flex items-center gap-[5px] font-semibold'>Blog<MdOutlineKeyboardArrowDown /></li>
-                <li className='flex items-center gap-[5px] font-semibold'>Elements<MdOutlineKeyboardArrowDown /></li>
+                <li className='flex items-center gap-[5px] font-semibold hover:text-[#FFD700] hover:cursor-pointer'>Home<MdOutlineKeyboardArrowDown /></li>
+                <li className='flex items-center gap-[5px] font-semibold hover:text-[#FFD700] hover:cursor-pointer'>Category<MdOutlineKeyboardArrowDown /></li>
+                <li className='flex items-center gap-[5px] font-semibold hover:text-[#FFD700] hover:cursor-pointer'>Products<MdOutlineKeyboardArrowDown /></li>
+                <li className='flex items-center gap-[5px] font-semibold hover:text-[#FFD700] hover:cursor-pointer'>Pages<MdOutlineKeyboardArrowDown /></li>
+                <li className='flex items-center gap-[5px] font-semibold hover:text-[#FFD700] hover:cursor-pointer'>Blog<MdOutlineKeyboardArrowDown /></li>
+                <li className='flex items-center gap-[5px] font-semibold hover:text-[#FFD700] hover:cursor-pointer'>Elements<MdOutlineKeyboardArrowDown /></li>
             </ul>
+              <a className='flex text-white items-center gap-[10px] hover:text-[#FFD700]' href="mailto:happiness9979@gmail.com">Email Us <MdOutlineMail /></a>
           </div>
         </div>
   )
--- src/app/Hero/Hero.jsx
+++ src/app/Hero/Hero.jsx
@@ -1,16 +1,20 @@
 "use client"
+import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 
 const Hero = () => {
   const hero = [
     {
-      name: "chrismi",
-      description: "hello world",
-      
+      name: "BUY YOU REAL GOLD HERE",
+      description: "PURCHASE YOUR TOPNOTCH QUALITY GOLD AT KRISPPY TODAY! ",
+      img: "/gold-bar.jpg",
+      btn: "Shop Now",
     },
     {
-      name: "happiness",
-      description: "Bf needed urgently",
+      name: "TESTED AND TRUSTED",
+      description: "WELCOME TO HOME OF QUALITY. WE ARE HAPPY TO MAKE YOU GLITTER",
+      img: "/images/hand-gold.avif",
+      btn: "Shop Now",
     },
   ];
 
@@ -21,15 +25,26 @@ const Hero = () => {
   }
 
   useEffect(() => {
-    const slideInterval = setInterval(handleslide, 5000); // Change slide every 5 seconds
-    return () => clearInterval(slideInterval); // Cleanup interval on component unmount
+    const slideInterval = setInterval(handleslide, 5000); 
+    return () => clearInterval(slideInterval); 
   }, []);
 
   return (
-    <div className='w-[100%] h-[70vh] bg-blue-500 flex items-center justify-center'>
-      <div>
-        <h1>{hero[currentSlide].name}</h1>
-        <p>{hero[currentSlide].description}</p>
+    <div className=' h-[70vh] bg-black flex items-center justify-center'>
+      <div className='flex justify-center w-[50%] items-center'>
+        <div className='flex flex-col gap-[20px] text-white'>
+          <h1 className='w-[100%] font-bold text-[25px]'>{hero[currentSlide].name}</h1>
+          <p className='w-[70%]'>{hero[currentSlide].description}</p>
+          <div className='bg-[#FFD700] w-[30%] text-center text-white py-[10px] font-bold rounded-[5px]'>
+             <button>{hero[currentSlide].btn}</button>
+          </div>
+        </div>
+        <div>
+           <Image src={hero[currentSlide].img} 
+            alt={hero[currentSlide].name} 
+            width={500} 
+            height={300} />
+        </div>
       </div>
     </div>
   );
