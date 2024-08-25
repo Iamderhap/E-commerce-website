96f24e468f4beffc944d9c8aad01a7a66849347e
unknown <happiness9979@gmail.com>
2024-08-25 18:58:21 +0200

    Section3 updated

--- src/app/Section2/Section2.jsx
+++ src/app/Section2/Section2.jsx
@@ -80,7 +80,7 @@ const Section2 = () => {
   return (
     <>
         <div className='flex flex-col items-center text-center gap-[40px] py-[40px]'>
-            <div className='w-[40vw] flex flex-col '>
+            <div className='w-[40vw] flex flex-col'>
                 <h1 className='font-bold text-[25px]'>Popular Products</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore lacus vel facilisis.</p>
             </div>
@@ -88,7 +88,7 @@ const Section2 = () => {
                 <div className='flex flex-col gap-[10px]'>
                     {
                         content.map((n,index)=>(
-                        <div className='flex items-center justify-between border-2 w-[20vw] py-[10px] px-[10px]' key={index}>
+                        <div className='flex items-center justify-between border-2 w-[20vw] py-[10px] px-[10px] rounded-lg' key={index}>
                             <p className='flex items-center'>{n.header}</p>
                             <p><FaArrowRight/></p>
                         </div>
@@ -96,14 +96,14 @@ const Section2 = () => {
                     }
 
                     <div className='w-[20vw]'>
-                        <Image className='h-[62vh]' src="/images/gold-bar2.jpg" width={500} height={300}/>
+                        <Image className='h-[62vh] rounded-lg' src="/images/gold-bar2.jpg" width={500} height={300}/>
                     </div>
                 </div>
                 <div className='h-[50vh] grid grid-cols-4 gap-[20px]'>
                     {
                         image.map((n,index)=>(
-                            <div key={index} className='border-2 w-[17vw] flex flex-col items-center text-center gap-[10px] py-[10px] bg-[#D3D3D3]'>
-                                <Image className='w-[15vw] h-[25vh] border-2' src={n.img} width={500} height={500}/>
+                            <div key={index} className='border-2 w-[17vw] flex flex-col items-center text-center gap-[10px] py-[10px] bg-[#D3D3D3] rounded-lg'>
+                                <Image className='w-[15vw] h-[25vh] border-2 rounded-lg' src={n.img} width={500} height={500}/>
                                 <p>{n.header}</p>
                                 <h3 className='w-[90%] font-semibold'>{n.content}</h3>
                                 <p className='font-bold flex gap-[10px]'>{n.number}<del>{n.del}</del></p>
--- src/app/Section3/Section3.jsx
+++ src/app/Section3/Section3.jsx
@@ -1,8 +1,75 @@
+import { Content } from 'next/font/google'
+import { headers } from 'next/headers'
+import { BiSupport } from "react-icons/bi";
 import React from 'react'
 
 const Section3 = () => {
+    const shop = [
+        {
+            headers: "Strong & healthy organic fruits",
+            Content: "35% off on first order",
+            btn: "Shop Now",
+        },
+        {
+            headers: "Strong & healthy organic fruits",
+            Content: "35% off on first order",
+            btn: "Shop Now",
+        },
+        {
+            headers: "Strong & healthy organic fruits",
+            Content: "35% off on first order",
+            btn: "Shop Now",
+        },
+    ]
+    const content2 = [
+        {
+            // icon: ""
+            header: "2X7 Support",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+        {
+            // icon: ""
+            header: "2X7 Support",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+        {
+            // icon: ""
+            header: "2X7 Support",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+        {
+            // icon: ""
+            header: "2X7 Support",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        },
+    ]
   return (
-    <div>Section3</div>
+    <>
+        <div className='flex flex-col gap-[100px]'>
+            <div className='flex justify-center gap-[20px] w-[100%]'>
+                {
+                   shop.map((s,index)=>(
+                 <div key={index} className="bg-[url('/images/fresh.jpg')] bg-cover bg-center px-[20px] rounded h-50 w-[30%] text-[red] flex flex-col gap-[10px] py-[50px]">
+                        <p className='w-[13vw] font-semibold text-[20px]'>{s.headers}</p>
+                        <p>{s.Content}</p>
+                        <button className='bg-[#FFD700] py-[10px] px-[15px] font-bold w-[30%]'>{s.btn}</button>
+                  </div>
+                    ))
+                }
+            </div>
+            <div className='flex justify-center gap-[20px]'>
+                {
+                    content2.map((n,index)=>(
+                        <div key={index} className='border-2 w-[20%] items-center flex text-center flex-col py-[20px] gap-[10px]'>
+                        <p><BiSupport /></p>
+                        <p>2X7 Support</p>
+                        <p className='w-[70%]'>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                </div>
+                    ))
+                }
+            </div>
+        </div>
+    </>
   )
 }
 
